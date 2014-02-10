8cf7ecd3895a302b235d3be0f9fa3c975a25c8e5
Guillem Marpons <gmarpons@babel.ls.fi.upm.es>
2014-02-10 10:30:57 +0100

    Read OldBankAccount from CSV

--- Guia/CsvImport.hs
+++ Guia/CsvImport.hs
@@ -5,22 +5,11 @@
   GADTs,
   NoImplicitPrelude,
   OverloadedStrings,
+  ScopedTypeVariables,
   TypeFamilies
   #-}
 
-module Guia.CsvImport
-       ( -- Utilities
-         sourceCsvFile
-
-         -- Debtor
-
-         -- Mandate
-
-         -- SpanishBank
-
-         -- SpanishBankAccount
-
-       ) where
+module Guia.CsvImport where
 
 import           ClassyPrelude
 import qualified Control.Monad.Trans.Error                                      as E
@@ -29,12 +18,16 @@ import qualified Control.Monad.Trans.State                                      
   (evalStateT, get, modify)
 import qualified Control.Monad.Trans.Resource                                   as R
   (MonadResource,
-   monadThrow)
+   monadThrow, runResourceT)
 import qualified Data.ByteString.Lazy                                           as LBS
   (hGetContents)
+import           Data.Conduit
+  ((=$), ($$))
 import qualified Data.Conduit                                                   as C
   (Source,
    bracketP, yield)
+import qualified Data.Conduit.List                                              as CL
+  (consume, iterM)
 import           Data.Csv
   ((.:))
 import qualified Data.Csv                                                       as CSV
@@ -42,7 +35,10 @@ import qualified Data.Csv                                                       
 import qualified Data.Csv.Streaming                                             as CSV
   (Records(Cons, Nil),
    decodeByName)
+import qualified Database.Persist.MongoDB                                       as DB
+  (insert_)
 import           Guia.Debtor
+import           Guia.MongoUtils
 import qualified System.IO                                                      as IO
   (FilePath, IOMode(ReadMode),
    openFile)
@@ -58,15 +54,6 @@ data CsvException
 instance Exception CsvException
 instance E.Error CsvException where
   strMsg = CsvError
-
-instance CSV.FromNamedRecord (Maybe SpanishBank) where
-  parseNamedRecord r = maybeSpanishBank <$> r .: "four_digits_code"
-                                        <*> r .: "bic"
-                                        <*> r .: "name"
-    where maybeSpanishBank fourDigitsCode bic name
-            | validSpanishBank fourDigitsCode bic name
-                = Just $ mkSpanishBank fourDigitsCode bic name
-            | otherwise = Nothing
 
 -- | Conduit's 'C.Source' for decoding CSV files. Every line/record is decoded into a
 -- value of type @o@. Data type @o@ needs to have an @instance FromNamedRecord (Maybe o)@
@@ -87,16 +74,55 @@ sourceCsvFile filePath =
     let eRecords = CSV.decodeByName byteString
     case eRecords of
       Left msg                 -> R.monadThrow (CsvHeaderException msg)
-      Right (_header, records) -> MS.evalStateT (yield' records) (1 :: Integer)
+      Right (_header, records) -> MS.evalStateT (yield' records) (2 :: Integer)
   where
     yield' (CSV.Cons (Left  msg)      _ ) = throw' CsvTypeConversionException msg
     yield' (CSV.Cons (Right (Just r)) rs) = lift (C.yield r) >> MS.modify (+1) >> yield' rs
     yield' (CSV.Cons (Right Nothing)  _ ) = throw' CsvDataValidationException ""
-    yield' (CSV.Nil  (Just msg)       _ ) = throw' CsvParsingException msg
+    yield' (CSV.Nil  (Just msg)       _ ) = throw' CsvParsingException (take 100 msg)
     yield' (CSV.Nil  Nothing          _ ) = return ()
     throw' e msg = do
       recordNum <- MS.get
       R.monadThrow $ e ("Line " ++ show recordNum ++ ": " ++ msg)
 
--- Example of insertion of a CSV file into a Mongo database:
--- >>> (_ :: [SpanishBank]) <- runResourceDbT $ sourceCsvFile "spanish_banks.csv" $$ CL.iterM DB.insert_ =$ CL.consume
+
+-- @OldBankAccount@'s
+
+readOldBankAccounts :: IO.FilePath -> IO [OldBankAccount]
+readOldBankAccounts filePath =
+  R.runResourceT $ sourceCsvFile filePath $$ CL.consume
+
+data OldBankAccount
+  = OldBankAccount
+    { id   :: Int
+    , iban :: Text }
+  deriving (Eq, Show, Read)
+
+instance CSV.FromNamedRecord (Maybe OldBankAccount) where
+  parseNamedRecord r = maybeOldBankAccount <$> r .: "id"
+                                           <*> r .: "bank_id"
+                                           <*> r .: "office"
+                                           <*> r .: "control_digits"
+                                           <*> r .: "num"
+    where maybeOldBankAccount id_ b o c n
+            | cccControlDigits b o n == c = Just $ OldBankAccount id_ (iban_ b o c n)
+            | otherwise                   = Nothing
+          iban_ b o c n = spanishIbanPrefixFromCcc (ccc b o c n) ++ ccc b o c n
+          ccc   b o c n = concat [b, o, c, n]
+
+-- @SpanishBank@'s
+
+importSpanishBanks :: IO.FilePath -> IO ()
+importSpanishBanks filePath = do
+  (_ :: [SpanishBank]) <-
+    runResourceDbT $ sourceCsvFile filePath $$ CL.iterM DB.insert_ =$ CL.consume
+  return ()
+
+instance CSV.FromNamedRecord (Maybe SpanishBank) where
+  parseNamedRecord r = maybeSpanishBank <$> r .: "four_digits_code"
+                                        <*> r .: "bic"
+                                        <*> r .: "name"
+    where maybeSpanishBank fourDigitsCode_ bic_ name
+            | validSpanishBank fourDigitsCode_ bic_ name
+                = Just $ mkSpanishBank fourDigitsCode_ bic_ name
+            | otherwise = Nothing
--- Guia/Debtor.hs
+++ Guia/Debtor.hs
@@ -13,26 +13,45 @@
 module Guia.Debtor
        ( -- Debtor
          Debtor, DebtorId,
+         firstName,
+         lastName,
+         activeMandate,
+         oldMandates,
+         registrationDate,
 
          -- Mandate
          Mandate, MandateId,
+         mandateReference,
+         account,
+         signatureDate,
+
+         -- SpanishBankAccount
+         SpanishBankAccount, SpanishBankAccountId,
+         iban,
+         bankId,
+         validSpanishBankAccount,
+         spanishIbanPrefixFromCcc,
+         cccControlDigits,
 
          -- SpanishBank
          mkSpanishBank,
          SpanishBank, SpanishBankId,
+         fourDigitsCode,
+         bic,
+         bankName,
          validSpanishBank,
          validSpanishBankBic,
-
-         -- SpanishBankAccount
-         SpanishBankAccount, SpanishBankAccountId,
 
          insertDebtor,
          cleanDebtors
        ) where
 
 import           ClassyPrelude
+import           Control.Lens
 import qualified Data.Char                                                      as CH
-  (isDigit, isUpper)
+  (digitToInt, intToDigit, isDigit, isUpper)
+import qualified Data.Text.Read                                                 as T
+  (decimal)
 import qualified Data.Time.Calendar                                             as C
   (Day)
 import qualified Database.Persist.MongoDB                                       as DB
@@ -42,32 +61,88 @@ import qualified Database.Persist.MongoDB                                       
 import qualified Database.Persist.Quasi                                         as DB
   (lowerCaseSettings)
 import qualified Database.Persist.TH                                            as DB
-  (mkPersist, persistFileWith, share)
+  (mkPersist, mpsGenerateLenses, mpsPrefixFields, persistFileWith, share)
 import           Guia.MongoSettings
 import           Guia.MongoUtils
+import qualified Text.Printf                                                    as PF
+  (printf)
 
-DB.share [DB.mkPersist mongoSettings]
+DB.share [DB.mkPersist mongoSettings { DB.mpsGenerateLenses = True
+                                     , DB.mpsPrefixFields   = False }]
   $(DB.persistFileWith DB.lowerCaseSettings "Guia/Debtor.persistent")
 
+validSpanishBankAccount :: Text -> SpanishBank -> Bool
+validSpanishBankAccount iban_ bank =
+     all CH.isDigit iban_ && length iban_ == 24
+  && ibanPrefix == spanishIbanPrefixFromCcc ccc
+  && controlDigits == cccControlDigits bankCode office num
+  && bank ^. fourDigitsCode == bankCode
+  where (ibanPrefix, ccc)    = splitAt 4 iban_
+        (bankCode, suffix)   = splitAt 4 ccc
+        (office, suffix')    = splitAt 4 suffix
+        (controlDigits, num) = splitAt 2 suffix'
+
+-- | Pre: 'ccc' contains exactly 20 decimal digits. Returned value contains a 4-character
+-- text, with "ES" prefix followed by 2 decimal digits.
+spanishIbanPrefixFromCcc :: Text -> Text
+spanishIbanPrefixFromCcc ccc = assert (all CH.isDigit ccc && length ccc == 20)
+                        $ "ES" ++ checkDigits
+  where
+    checkDigits = pack $ PF.printf "%02d" (98 - (extendedCccAsNum `mod` 97))
+    extendedCccAsNum :: Integer                          -- We need unbound precision
+    extendedCccAsNum = case extendedCccAsEither of
+      (Left msg)                            -> error $ "spanishIbanPrefix: " ++ msg
+      (Right (num, text)) | not (null text) -> error "spanishIbanPrefix: bad decimal"
+                          | otherwise       -> num
+    -- 14 is the code for "E", and "28" is the code for "S", see
+    -- http://en.wikipedia.org/wiki/International_Bank_Account_Number#Generating_IBAN_check_digits
+    extendedCccAsEither = T.decimal $ ccc ++ "14" ++ "28" ++ "00"
+
+-- | Pre: 'bankId' contains exactly 4 decimal digits.
+-- Pre: 'office' contains exactly 4 decimal digits.
+-- Pre: 'num' contains exactly 10 decimal digits.
+-- Returned value contains exactly 2 decimal digits.
+cccControlDigits :: Text -> Text -> Text -> Text
+cccControlDigits bankCode office num =
+  assert (    all CH.isDigit bankCode && length bankCode ==  4
+           && all CH.isDigit office && length office ==  4
+           && all CH.isDigit num    && length num    == 10 )
+  $ pack [firstDigit, secondDigit]
+  where
+    -- CH.digitToInt fails if not isHexDigit
+    firstDigit                        = cccDigit (bankCode ++ office)
+    secondDigit                       = cccDigit num
+    cccDigit                          = intsToDigit . stringToInts . unpack
+    stringToInts str                  = reverse $ map CH.digitToInt str
+    intsToDigit                       = intToDigit' . intsToInt
+    intsToInt ints                    =  11 - (sum (zipWith (*) weights ints) `mod` 11)
+    weights                           = [6, 3, 7, 9, 10, 5, 8, 4, 2, 1] :: [Int]
+    intToDigit'                       :: Int -> Char
+    intToDigit' 10                    = '1'
+    intToDigit' 11                    = '0'
+    intToDigit' x | x >= 0 && x <= 9  = CH.intToDigit x -- would fail with x >= 16
+    intToDigit' _x                    = error "cccControlDigits: Int >= 11"
+
 mkSpanishBank :: Text -> Text -> Text -> SpanishBank
-mkSpanishBank fourDigitsCode bic name =
-  assert (validSpanishBank fourDigitsCode bic name) (SpanishBank fourDigitsCode bic name)
+mkSpanishBank fourDigitsCode_ bic_ bankName_ =
+  assert (validSpanishBank fourDigitsCode_ bic_ bankName_)
+  $ SpanishBank fourDigitsCode_ bic_ bankName_
 
 validSpanishBank :: Text -> Text -> Text -> Bool
-validSpanishBank fourDigitsCode bic name =
-     length fourDigitsCode == 4
-  && all CH.isDigit fourDigitsCode
-  && validSpanishBankBic bic
-  && length name <= maxSpanishBankName
-  where maxSpanishBankName = 100
+validSpanishBank fourDigitsCode_ bic_ bankName_ =
+     length fourDigitsCode_ == 4
+  && all CH.isDigit fourDigitsCode_
+  && validSpanishBankBic bic_
+  && length bankName_ <= maxBankName
+  where maxBankName = 100
 
 validSpanishBankBic :: Text -> Bool
-validSpanishBankBic bic =    length bic == 11
+validSpanishBankBic bic_ =   length bic_ == 11
                           && all CH.isUpper institution
                           && country == "ES"
                           && all isDigitOrUpper location
                           && all isDigitOrUpper branch
-  where (institution, suffix) = splitAt 4 bic
+  where (institution, suffix) = splitAt 4 bic_
         (country, suffix')    = splitAt 2 suffix
         (location, branch)    = splitAt 2 suffix'
         isDigitOrUpper c      = CH.isDigit c || CH.isUpper c
