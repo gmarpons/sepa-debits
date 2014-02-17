e5cb7a5e4cafd01842f29b2db7ea386d40ef0d0b
Guillem Marpons <gmarpons@babel.ls.fi.upm.es>
2014-02-17 09:14:37 +0100

    Load glade file and quit pressing Exit button

--- GuiaDirectDebits.hs
+++ GuiaDirectDebits.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import           Graphics.UI.Gtk
+
 main :: IO ()
-main = return ()
+main = do
+  initGUI
+  builder <- builderNew
+  builderAddFromFile builder "glade/GuiaDirectDebits.glade"
+  mainWd <- builderGetObject builder castToWindow "mainWd"
+  mainVb <- builderGetObject builder castToVBox "mainVb"
+  mwExitBt <- builderGetObject builder castToButton "mwExitBt"
+  on mwExitBt buttonActivated $ widgetDestroy mainWd >> mainQuit
+  widgetShowAll mainWd
+  mainGUI
+
