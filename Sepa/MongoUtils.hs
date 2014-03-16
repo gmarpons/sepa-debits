b4bdc4dc8c9b3c924048b9c68dd97f86f860ee4d
Guillem Marpons <gmarpons@babel.ls.fi.upm.es>
2014-03-16 17:41:33 +0100

    Application state threaded down using ReaderT
    App. state data types created with Lens interface.

--- Sepa/MongoUtils.hs
+++ Sepa/MongoUtils.hs
@@ -1,31 +1,27 @@
-{-# LANGUAGE
-  FlexibleContexts,
-  NoImplicitPrelude,
-  OverloadedStrings
-  #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
 
 module Sepa.MongoUtils where
 
 import           ClassyPrelude
-import qualified Control.Monad.Trans.Resource                                   as R
-  (MonadBaseControl, ResourceT,
-   runResourceT)
-import qualified Data.Time.Clock                                                as C
-  (NominalDiffTime)
-import qualified Database.Persist.MongoDB                                       as DB
-  (Action, Database, HostName,
-   master, withMongoDBConn, runMongoDBPool)
-import qualified Network                                                        as N
-  (PortID(PortNumber))
+import qualified Control.Monad.Trans.Resource as R (MonadBaseControl, ResourceT,
+                                                    runResourceT)
+import qualified Data.Time.Clock              as C (NominalDiffTime)
+import qualified Database.Persist.MongoDB     as DB (Action, Database, HostName,
+                                                     master, runMongoDBPool,
+                                                     withMongoDBConn)
+import qualified Network                      as N (PortID (PortNumber))
 
 runDb :: (MonadIO m, R.MonadBaseControl IO m) => DB.Action m a -> m a
 runDb action = DB.withMongoDBConn dbname hostname port Nothing time runAction
   where
     runAction = DB.runMongoDBPool DB.master action
-    dbname = "test" :: DB.Database
-    hostname = "localhost" :: DB.HostName
-    port = N.PortNumber 27017 :: N.PortID
-    time = 2 :: C.NominalDiffTime -- 1 hour before close connection
+    dbname    = "test" :: DB.Database
+    hostname  = "localhost" :: DB.HostName
+    port      = N.PortNumber 27017 :: N.PortID
+    time      = 2 :: C.NominalDiffTime
 
 runResourceDbT :: (MonadIO m, R.MonadBaseControl IO m) =>
                   R.ResourceT (DB.Action m) a -> m a
--- SepaDebits.hs
+++ SepaDebits.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
 
 module Main
        ( main
@@ -8,29 +9,19 @@ module Main
 
 import           Control.Lens             hiding (set, view)
 import           Control.Monad
+import           Data.IORef
 import           Data.List
-import qualified Data.Text                as T  (pack, replace, unpack)
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Reader
+import qualified Data.Text                as T (pack, replace, unpack)
 import qualified Data.Text.Lazy           as TL (unpack)
+import qualified Data.Time.Clock          as C (NominalDiffTime)
 import qualified Database.Persist.MongoDB as DB
 import           Formatting               hiding (builder)
 import           Graphics.UI.Gtk
+import qualified Network                  as N (PortID (PortNumber))
 import           Sepa.BillingConcept
-import           Sepa.MongoUtils
---import qualified System.Glib.GObject         as G
-
-
-main :: IO ()
-main = do
-  _ <- initGUI
-  builder <- builderNew
-  builderAddFromFile builder "glade/SepaDebits.glade"
-  mainWd <- builderGetObject builder castToWindow "mainWd"
-  mwExitBt <- builderGetObject builder castToButton "mwExitBt"
-  _ <- on mwExitBt buttonActivated $ widgetDestroy mainWd >> mainQuit
-  _ <- on mainWd objectDestroy mainQuit -- FIXME: don't exit if dirty state
-  mkGui builder
-  widgetShowAll mainWd
-  mainGUI
+import qualified System.Glib.GObject      as G
 
 type PanelId = String
 type PanelDescr = (PanelId, (String, String))
@@ -42,85 +33,164 @@ panels =
   , ("directDebits",    ("directDebitsTb",    "directDebitsVb"   ))
   ]
 
-mkGui :: Builder -> IO ()
-mkGui builder = do
-  -- Main panel widgets
+data AppState
+  = AppState
+    { _guiSt   :: GuiState
+    , _builder :: Builder
+    , _db      :: DB.ConnectionPool
+    }
+
+data GuiState
+  = View { _panelId :: PanelId, _panelSt :: PanelState }
+  | Edit { _panelId :: PanelId, _panelSt :: PanelState }
+  deriving Show
+
+data PanelState
+  = NoSel
+  | Sel     { _item :: TreeIter }
+  | EditNew { _data :: [String], _isValid :: Bool }
+  | EditOld { _item :: TreeIter, _isValid :: Bool }
+  deriving Show
+
+makeLenses ''AppState
+makeLenses ''GuiState
+makeLenses ''PanelState
+
+type App = ReaderT (IORef AppState) IO
+
+askAppSt :: App AppState
+askAppSt = do
+  appStRef <- ask
+  liftIO $ readIORef appStRef
+
+main :: IO ()
+main = do
+  _ <- initGUI
+  db_ <- DB.createMongoDBPool dbName hostName port Nothing poolSize stripeSize time
+  builder_ <- builderNew
+  builderAddFromFile builder_ gladeFile
+  let initialAppState = AppState (View "billingConcepts" NoSel) builder_ db_
+  appStRef <- newIORef initialAppState
+  mainWd <- runReaderT mkGui appStRef
+  widgetShowAll mainWd
+  mainGUI
+    where
+      gladeFile  = "glade/SepaDebits.glade"
+      dbName     = "test" :: DB.Database
+      hostName   = "localhost" :: DB.HostName
+      port       = N.PortNumber 27017 :: N.PortID -- Standard MongoDB port
+      poolSize   = 10                             -- Num. of stripes
+      stripeSize = 10                             -- Num. of connections per stripe
+      time       = 5 :: C.NominalDiffTime         -- Seconds
+
+mkGui :: App Window
+mkGui = do
+  st <- askAppSt
+  let bldr = st ^. builder
+
+  -- Main window widgets
+
   let panelIds          = (fst . unzip)               panels
   let panelChooserNames = (fst . unzip . snd . unzip) panels
   let panelVBoxes       = (snd . unzip . snd . unzip) panels
+  mainWd <- liftIO $ builderGetObject bldr castToWindow "mainWd"
+  mwExitBt <- liftIO $ builderGetObject bldr castToButton "mwExitBt"
+  _ <- liftIO $ on mwExitBt buttonActivated $ widgetDestroy mainWd >> mainQuit
+  _ <- liftIO $ on mainWd objectDestroy mainQuit -- FIXME: don't exit if dirty state
   panelChoosers <- forM panelChooserNames $ \name -> do
-    widget <- builderGetObject builder castToToggleButton name
-    widgetSetName widget name
+    widget <- liftIO $ builderGetObject bldr castToToggleButton name
+    liftIO $ widgetSetName widget name
     return widget
-  panelBoxes <- mapM (builderGetObject builder castToVBox) panelVBoxes
-  mainVb <- builderGetObject builder castToVBox "mainVb"
-  forM_ panelChoosers $ \chooser -> on chooser toggled $ do
-    isActive <- toggleButtonGetActive chooser
+  panelBoxes <- mapM (liftIO . builderGetObject bldr castToVBox) panelVBoxes
+  mainVb <- liftIO $ builderGetObject bldr castToVBox "mainVb"
+  forM_ panelChoosers $ \chooser -> liftIO $ on chooser toggled $ do
+    isActive <- liftIO $ toggleButtonGetActive chooser
     when isActive $ do
-      chooserName <- widgetGetName chooser
-      putStrLn "toggled"
-      (oldBox : _) <- containerGetChildren mainVb -- FIXME: unsafe pattern
+      chooserName <- liftIO $ widgetGetName chooser
+      liftIO $ putStrLn "toggled"
+      (oldBox : _) <- liftIO $ containerGetChildren mainVb -- FIXME: unsafe pattern
       let otherChoosers = filter (/= chooser) panelChoosers
       let mbNewBox = lookup chooserName $ zip panelChooserNames panelBoxes
       let (Just newBox) = mbNewBox
-      set chooser [widgetSensitive := False]
-      forM_ otherChoosers $ \o -> set o [ toggleButtonActive := False
-                                        , widgetSensitive := True ]
-      containerRemove mainVb oldBox
-      boxPackStart mainVb newBox PackGrow 0
-  -- Sub-panel widgets
-  mkBillingConceptsGui builder
-  return ()
+      liftIO $ set chooser [widgetSensitive := False]
+      forM_ otherChoosers $ \o -> liftIO $ set o [ toggleButtonActive := False
+                                                 , widgetSensitive := True ]
+      liftIO $ containerRemove mainVb oldBox
+      liftIO $ boxPackStart mainVb newBox PackGrow 0
 
-mkBillingConceptsGui :: Builder -> IO ()
-mkBillingConceptsGui builder = do
-  billingConceptsTv <- builderGetObject builder castToTreeView "billingConceptsTv"
-  editBillingConceptTb <- builderGetObject builder castToToggleButton "editBillingConceptTb"
-  newBillingConceptTb  <- builderGetObject builder castToToggleButton "newBillingConceptTb"
-  deleteBillingConceptBt <- builderGetObject builder castToButton "deleteBillingConceptBt"
-  billingConceptDescriptionEn <- builderGetObject builder castToEntry "billingConceptDescriptionEn"
-  billingConceptBasePriceEn <- builderGetObject builder castToEntry "billingConceptBasePriceEn"
-  billingConceptVatRatioEn <- builderGetObject builder castToEntry "billingConceptVatRatioEn"
-  billingConceptFinalPriceEn <- builderGetObject builder castToEntry "billingConceptFinalPriceEn"
-  saveBillingConceptBt <- builderGetObject builder castToButton "saveBillingConceptBt"
-  cancelBillingConceptBt <- builderGetObject builder castToButton "cancelBillingConceptBt"
-  billingConceptsTm <- builderGetObject builder castToTreeModel "billingConceptsLs"
+  -- Sub-panel widgets
+
+  mkBillingConceptsGui
+  return mainWd
+
+mkBillingConceptsGui :: App ()
+mkBillingConceptsGui = do
+  st <- askAppSt
+
   -- Populate tree view
-  billingConceptsEL <- runDb $ DB.selectList ([] :: [DB.Filter BillingConcept]) []
-  billingConceptsLs <- listStoreNew billingConceptsEL
-  billingConceptsSm <- treeModelSortNewWithModel billingConceptsLs
-  -- lsSize <- listStoreGetSize billingConceptsLs
-  -- putStrLn $ "Number of billing concepts: " ++ show lsSize
-  treeViewSetModel billingConceptsTv billingConceptsSm
-  billingConceptsCols <- treeViewGetColumns billingConceptsTv
+
+  billingConceptsTv <- liftIO $ builderGetObject (st ^. builder) castToTreeView "billingConceptsTv"
+  billingConceptsEL <- flip DB.runMongoDBPoolDef (st ^. db) $
+                       DB.selectList ([] :: [DB.Filter BillingConcept]) []
+  -- Setting tree view models from glade doesn't work: stablish connection here and we can
+  -- use treeViewGet* functions afterwards.
+  -- billingConceptsLs <- treeViewGetListStore billingConceptsTv
+  -- mapM_ (listStoreAppend billingConceptsLs) billingConceptsEL
+  billingConceptsLs <- liftIO $ listStoreNew billingConceptsEL
+  billingConceptsSm <- liftIO $ treeModelSortNewWithModel billingConceptsLs
+  liftIO $ treeViewSetModel billingConceptsTv billingConceptsSm
+
   let renderFuncs = [ T.unpack      . (^. longName)   . DB.entityVal
                     , T.unpack      . (^. shortName)  . DB.entityVal
                     , priceToString . (^. basePrice)  . DB.entityVal
                     , priceToString . (^. vatRatio)   . DB.entityVal
                     , priceToString . (^. finalPrice) . DB.entityVal
                     ]
+
   -- forall columns: set renderer, set sorting func
+
+  billingConceptsCols <- liftIO $ treeViewGetColumns billingConceptsTv
   forM_ (zip3 billingConceptsCols renderFuncs [0..]) $ \(col, renderFunc, colId) -> do
     -- FIXME: column manual resizing doesn't work
     let cellLayout = toCellLayout col
-    (cell : _) <- cellLayoutGetCells cellLayout    -- FIXME: unsafe pattern, dep. on glade
-    let textRenderer = castToCellRendererText cell -- FIXME: unsafe cast, depends on glade
-    cellLayoutSetAttributes col textRenderer billingConceptsLs $ \row ->
+    (cell : _) <- liftIO $ cellLayoutGetCells cellLayout    -- FIXME: unsafe pattern, dep. on glade
+    let textRenderer = castToCellRendererText cell          -- FIXME: unsafe cast, depends on glade
+    liftIO $ cellLayoutSetAttributes col textRenderer billingConceptsLs $ \row ->
       [ cellText := renderFunc row ]
     let sortFunc xIter yIter = do
-          xRow <- customStoreGetRow billingConceptsLs xIter
-          yRow <- customStoreGetRow billingConceptsLs yIter
+          xRow <- liftIO $ customStoreGetRow billingConceptsLs xIter
+          yRow <- liftIO $ customStoreGetRow billingConceptsLs yIter
           return $ compare (renderFunc xRow) (renderFunc yRow)
-    treeSortableSetSortFunc billingConceptsSm colId sortFunc
-    treeViewColumnSetSortColumnId col colId
+    liftIO $ treeSortableSetSortFunc billingConceptsSm colId sortFunc
+    liftIO $ treeViewColumnSetSortColumnId col colId
+
   -- Incremental search in tree view
+
   -- TODO: accent-independent search or TreeModelFilter
-  let equalFunc text iter = do
-        childIter <- treeModelSortConvertIterToChildIter billingConceptsSm iter
-        row <- customStoreGetRow billingConceptsLs childIter
-        return $ any (\f -> text `isInfixOf` f row) renderFuncs
-  treeViewSetSearchEqualFunc billingConceptsTv (Just equalFunc)
+  let equalFunc text_ iter = do
+        childIter <- liftIO $ treeModelSortConvertIterToChildIter billingConceptsSm iter
+        row <- liftIO $ customStoreGetRow billingConceptsLs childIter
+        return $ any (\f -> text_ `isInfixOf` f row) renderFuncs
+  liftIO $ treeViewSetSearchEqualFunc billingConceptsTv (Just equalFunc)
   return ()
+
+-- | Possibly unsafe operation. Error if @view@ doesn't have a TreeModelSort model.
+treeViewGetSortedModel :: forall a . TreeView -> IO (TypedTreeModelSort a)
+treeViewGetSortedModel view = do
+  (Just uncastedTreeModelSort) <- treeViewGetModel view
+  let treeModelSort = (G.unsafeCastGObject . toGObject) uncastedTreeModelSort
+                      :: TypedTreeModelSort a
+  return treeModelSort
+
+-- | Possibly unsafe operation. Error if @view@ doesn't have a ListStore model.
+treeViewGetListStore :: forall a . TreeView -> IO (ListStore a)
+treeViewGetListStore view = do
+  (Just uncastedTreeModelSort) <- treeViewGetModel view
+  let treeModelSort = (G.unsafeCastGObject . toGObject) uncastedTreeModelSort
+                      :: TypedTreeModelSort a
+  uncastedListStore <- treeModelSortGetModel treeModelSort
+  let listStore = (G.unsafeCastGObject . toGObject) uncastedListStore
+  return listStore
 
 priceToString :: Int -> String
 priceToString num = TL.unpack $ format (left padding ' ') (toText num)
