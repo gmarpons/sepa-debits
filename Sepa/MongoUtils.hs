f01024202edd2bf9cff3f332206c9d9eff068c60
Guillem Marpons <gmarpons@babel.ls.fi.upm.es>
2014-03-24 21:04:25 +0100

    Rename DB to "sepadebits" in MongoUtils.hs

--- Sepa/MongoUtils.hs
+++ Sepa/MongoUtils.hs
@@ -18,8 +18,8 @@ runDb :: (MonadIO m, R.MonadBaseControl IO m) => DB.Action m a -> m a
 runDb action = DB.withMongoDBConn dbname hostname port Nothing time runAction
   where
     runAction = DB.runMongoDBPool DB.master action
-    dbname    = "test" :: DB.Database
-    hostname  = "localhost" :: DB.HostName
+    dbname    = "sepadebits" :: DB.Database
+    hostname  = "localhost"  :: DB.HostName
     port      = N.PortNumber 27017 :: N.PortID
     time      = 2 :: C.NominalDiffTime
 
